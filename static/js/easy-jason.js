627dbbca74bacb8e7f007e66889873597c6f1495
lijeffrey39 <lijeffrey39@gmail.com>
2020-02-26 16:04:01 -0500

    bitch shift

--- static/js/easy-jason.js
+++ static/js/easy-jason.js
@@ -1,20 +1,29 @@
 console.log("i'm a cunt");
 console.log("sfdjlksd");
-// import { Synth } from "tone";
-// console.log("sdlkfj");
-// import * as Tone from "tone";
 
 //create a synth and connect it to the master output (your speakers)
-const synth = new Tone.AMSynth().toMaster();
+const synth = new Tone.Synth().toMaster();
+// var pitchShift = new Tone.PitchShift({
+//     pitch: -1,
+//     windowSize: 0.1
+// }).toMaster();
 
 //play a middle 'C' for the duration of an 8th note
 // synth.triggerAttackRelease("C4", "8n");
 var box = document.getElementById('box');
-console.log('joeee');
 box.addEventListener("click", e => {
     synth.triggerAttack('C4');
     console.log('buietch');
 })
 
-
-// console.log("wat");
+var count = 0.1
+var box1 = document.getElementById('box1');
+box1.addEventListener("click", e => {
+    synth.disconnect()
+    var pitchShift = new Tone.PitchShift({
+        pitch: count
+        // windowSize: 
+    }).toMaster();
+    synth.connect(pitchShift);
+    count += 0.1;
+})
