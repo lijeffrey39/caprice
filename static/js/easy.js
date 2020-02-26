5eedcdc45b97e799a198fe10206199c1c93653bc
Jaosn <jhsu2@andrew.cmu.edu>
2020-02-26 15:27:27 -0500

    sound

--- static/js/easy.js
+++ static/js/easy.js
@@ -2,12 +2,19 @@ console.log("i'm a cunt");
 console.log("sfdjlksd");
 // import { Synth } from "tone";
 // console.log("sdlkfj");
-import * as Tone from "tone";
+// import * as Tone from "tone";
 
 //create a synth and connect it to the master output (your speakers)
-const synth = new Tone.Synth().toDestination();
+const synth = new Tone.AMSynth().toMaster();
 
 //play a middle 'C' for the duration of an 8th note
-synth.triggerAttackRelease("C4", "8n");
+// synth.triggerAttackRelease("C4", "8n");
+var box = document.getElementById('box');
+console.log('joeee');
+box.addEventListener("click", e => {
+    synth.triggerAttack('C4');
+    console.log('buietch');
+})
+
 
 // console.log("wat");
